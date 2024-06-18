7c5de04bbc9969b1996a00732250b733e3b1dfe6
Juan Vieira <juanvieira1@yahoo.com>
2024-06-18 12:32:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Projeto de Ciência de Dados - Previsão de Doenças Cardíacas
+
+Este projeto visa desenvolver um algoritmo de Machine Learning para prever a tendência de uma pessoa desenvolver algum tipo de doença cardíaca com base em fatores clínicos e laboratoriais. Utilizou-se um dataset disponível no Kaggle, acessível através do link [Heart Failure Prediction Dataset](https://www.kaggle.com/datasets/fedesoriano/heart-failure-prediction).
+
+## Etapas do Projeto
+
+1. **Análise Exploratória de Dados (EDA)**
+   - O arquivo `eda.ipynb` foi utilizado para explorar visualizações dos dados utilizando as bibliotecas Seaborn, Plotly e Pandas.
+
+2. **Limpeza e Tratamento de Dados**
+   - No arquivo `cleaning.ipynb`, os dados foram limpos e tratados para remoção de valores ausentes, tratamento de outliers e padronização dos dados utilizando Pandas e NumPy.
+
+3. **Pré-processamento dos Dados**
+   - O pré-processamento dos dados foi realizado no arquivo `train.py`, onde os dados foram preparados para o treinamento do modelo. Isso incluiu a codificação de variáveis categóricas, normalização ou padronização de variáveis numéricas e divisão do conjunto de dados em treino e teste. Foram utilizadas as bibliotecas Scikit-learn, Pandas e Pickle para serialização do modelo.
+
+4. **Treinamento do Modelo**
+   - Utilizando o `train.py`, o modelo foi treinado com os dados preparados na etapa anterior. Foi aplicada validação cruzada para avaliar a performance do modelo e otimizar os parâmetros.
+
+5. **Teste do Modelo** (etapa atual)
+   - Após o treinamento, o modelo foi testado utilizando o conjunto de teste separado. Métricas de avaliação foram calculadas para verificar a precisão e eficácia do modelo.
+
+6. **Previsão**
+   - Ainda será feita.
+
+## Arquivos
+
+- `eda.ipynb`: Notebook utilizado para visualizações exploratórias e entendimento inicial dos dados utilizando Seaborn, Plotly e Pandas.
+- `cleaning.ipynb`: Notebook utilizado para a limpeza e tratamento inicial dos dados, utilizando Pandas e NumPy.
+- `train.py`: Script Python utilizado para o pré-processamento, treinamento, avaliação e validação cruzada do modelo de Machine Learning. Utilizou-se Scikit-learn, Pandas e Pickle para serialização do modelo.
+- Pasta `data`: Contém os arquivos de dados utilizados no projeto.
+
+## Contribuição
+
+Este projeto está em constante evolução. Contribuições são bem-vindas! Sinta-se à vontade para abrir issues ou enviar pull requests com melhorias e sugestões.
---
+++ src/model/train.py
@@ -0,0 +1,179 @@
+# %%
+import pickle
+import datetime
+
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.decomposition import KernelPCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import KFold, cross_val_score
+
+
+# %%
+df = pd.read_csv('../../data/heart_cleaned.csv', delimiter=';')
+df.head(3)
+# %%
+df2 = pd.DataFrame.copy(df)
+
+df2['Sex'].replace({'M': 0, 'F': 1}, inplace=True)
+df2['ChestPainType'].replace({'TA':0, 'ATA':1, 'NAP':2, 'ASY': 3}, inplace=True)
+df2['RestingECG'].replace({'Normal':0, 'ST':1, 'LVH':2}, inplace=True)
+df2['ExerciseAngina'].replace({'N':0, 'Y':1}, inplace=True)
+df2['ST_Slope'].replace({'Up':0, 'Flat':1, 'Down':2}, inplace=True)
+
+# %%
+df2.head()
+
+# %%
+pred = df2.iloc[:, 0:11].values
+pred
+
+# %%
+pred.shape
+
+# %%
+target = df2.iloc[:, 11].values
+target
+
+# %%
+target.shape
+
+# %%
+pred_esc = StandardScaler().fit_transform(pred)
+pred_esc
+
+# %%
+pred_df = pd.DataFrame(pred_esc)
+pred_df
+
+# %%
+pred_df.describe()
+
+# %%
+pred2 = df.iloc[:, 0:11].values
+pred2
+
+# %%
+pred2[:,1] = LabelEncoder().fit_transform(pred[:,1])
+pred2
+
+# %%
+pred2[:,2] = LabelEncoder().fit_transform(pred[:,2])
+pred2[:,6] = LabelEncoder().fit_transform(pred[:,6])
+pred2[:,8] = LabelEncoder().fit_transform(pred[:,8])
+pred2[:,10] = LabelEncoder().fit_transform(pred[:,10])
+
+pred2
+
+# %%
+pred3 = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(),
+                                         [1,2,6,8,10])],
+                                         remainder='passthrough').fit_transform(pred2)
+
+# %%
+pred3.shape
+
+# %%
+pred3esc = StandardScaler().fit_transform(pred3)
+pred3esc
+
+# %%
+pca = PCA(n_components=4)
+predictors_pca = pca.fit_transform(pred2)
+
+predictors_pca.shape
+
+# %%
+pca.explained_variance_ratio_
+
+# %%
+kpca = KernelPCA(n_components=4, kernel='rbf')
+predictors_kernel = kpca.fit_transform(pred2)
+
+predictors_kernel.shape
+
+# %%
+lda = LinearDiscriminantAnalysis(n_components=1)
+predictors_lda = lda.fit_transform(pred2, target)
+
+predictors_lda.shape
+
+# %%
+lda.explained_variance_ratio_
+
+# %%
+arq = open('heart.pkl', 'wb')
+pickle.dump(target, arq)
+arq2 = open('heart2.pkl', 'wb')
+pickle.dump(pred, arq2)
+arq3 = open('heart3.pkl', 'wb')
+pickle.dump(pred_esc, arq3)
+arq4 = open('heart4.pkl', 'wb')
+pickle.dump(pred2, arq4)
+arq5 = open('heart5.pkl', 'wb')
+pickle.dump(pred3, arq5)
+arq6 = open('heart6.pkl', 'wb')
+pickle.dump(pred3esc, arq6)
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(pred3esc,
+                                                    target, 
+                                                    test_size=0.3,
+                                                    random_state=0)
+
+X_train.shape
+
+# %%
+X_test.shape
+
+# %%
+y_test.shape
+
+# %%
+y_train.shape
+
+# %%
+naive = GaussianNB()
+naive.fit(X_train, y_train)
+
+# %%
+pred_naive = naive.predict(X_test)
+pred_naive
+
+# %%
+accuracy_score(y_test, pred_naive)
+
+# %%
+confusion_matrix(y_test, pred_naive)
+
+# %%
+print(classification_report(y_test, pred_naive))
+
+# %%
+pred_train = naive.predict(X_train)
+pred_train
+
+# %%
+accuracy_score(y_train, pred_train)
+
+# %%
+confusion_matrix(y_train, pred_train)
+
+# %%
+kfold = KFold(n_splits = 30, shuffle=True, random_state=5)
+
+# %%
+model = GaussianNB()
+result = cross_val_score(model, pred3esc, target, cv = kfold)
+
+result.mean()
